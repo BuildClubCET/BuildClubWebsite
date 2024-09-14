c92f74b3408e42902a353ac52775e3ee541357a1
Ivin <ivinmk2410@hotmail.com>
2024-09-14 23:00:30 +0530

    Base project with tailwind

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="font-extrabold text-3xl text-red-500 border-2 border-black">
+        Test
+    </div>
+    </>
+  )
+}
+
+export default Navbar
