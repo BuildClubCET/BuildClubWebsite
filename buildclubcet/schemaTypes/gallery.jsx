0938b062c6068238412de888fcb06070ef728f7e
Ivin <ivinmk2410@hotmail.com>
2025-02-09 21:44:03 +0530

    Set up sanity and gallery document

---
+++ buildclubcet/schemaTypes/gallery.jsx
@@ -0,0 +1,25 @@
+import {defineType, defineField} from "sanity"
+
+export const gallery = defineType({
+    name:"Gallery",
+    title:"Gallery",
+    type:"document",
+    fields:[
+        defineField({
+            name:"Title",
+            type:"string",
+            validation: (rule) => rule.required()
+        }),
+        defineField({
+            name:"Image",
+            type:"image",
+            validation: (rule) => rule.required()
+        }),
+
+    ]
+})
+
+/* 
+    Query to get the image title and url
+    *[_type=="Gallery"]{_id, Title, "imageURL":Image.asset->url}
+*/
